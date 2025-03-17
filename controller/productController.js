fe969c54a4867d306920ef4b4e5bd085e3b4253b
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-18 02:17:34 +0330

    [add] Implement product controller with CRUD operations for products

---
+++ controller/productController.js
@@ -0,0 +1,56 @@
+const Product = require('../models/Product');
+const { v4: uuidv4 } = require('uuid');
+
+exports.createProduct = async (req, res, next) => {
+    try {
+        const { name, description, price, stock } = req.body;
+        const product = await new Product({ id: uuidv4(), name, description, price, stock });
+        res.status(201).json(product);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.getAllProducts = async (req, res, next) => {
+    try {
+        const product = await Product.find();
+        if (!product) throw new Error("Product not found");
+        res.json(product);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.getProductById = async (req, res, next) => {
+    try {
+        const productSearch = await Product.findById(req.params.id);
+        if (!productSearch) throw new Error("Product not found");
+        res.json(productSearch);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.updateProduct = async (req, res, next) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!updatedProduct) throw new Error("Product not found");
+        res.json(updatedProduct);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.deleteProduct = async (req, res, next) => {
+    try {
+        const productDeleted = await Product.findByIdAndDelete(req.params.id);
+        if (!productDeleted) throw new Error("Product not found");
+        res.json(productDeleted);
+    } catch (error) {
+        next(error);
+    }
+}
