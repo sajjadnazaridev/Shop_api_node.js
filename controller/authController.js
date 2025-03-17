9f524a036ebd9cf3e426e7c574504126821a1f5d
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-18 01:00:20 +0330

    [add] Implement authentication controller with user registration and login methods

---
+++ controller/authController.js
@@ -0,0 +1,21 @@
+const authService = require("../services/authService");
+
+exports.register = async (req, res, next) => {
+    try {
+        const { username, password, role } = req.body;
+        const user = await authService.registerUser(username, password, role);
+        res.status(201).json({ message: "User registered successfully" }, user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const token = await authService.loginUser(username, password);
+        res.json({ token });
+    } catch (error) {
+        next(error);
+    }
+}
