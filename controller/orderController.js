8f55364c088631c757ce6759dcee9d768d5921d3
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-18 17:30:02 +0330

    [add] Implement order controller with create, update, and get order history functionalities

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,1 +1,30 @@
 const orderService = require("../services/orderService");
+
+exports.createOrder = async (req, res, next) => {
+    try {
+
+        const order = await orderService.createOrder(req.body);
+        res.status(201).json(order);
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+exports.updateOrder = async (req, res, next) => {
+    try {
+        const updatedOrder = await orderService.updateOrder(req.params.id, req.body);
+        res.json(updatedOrder);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.getOrderHIstory = async (req, res, next) => {
+    try {
+        const getHistory = await orderService.getOrderHistory(req.params.id);
+        res.json(getHistory);
+    } catch (error) {
+        next(error);
+    }
+}
