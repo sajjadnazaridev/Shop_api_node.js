88956224c0cb9902370c4edef17019aa1e05bca5
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-18 19:37:42 +0330

    [add] Integrate Winston for logging and enhance error handling in the application

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
+const logger = require("./logger");
 const app = express();
 
 app.use(express.json());
 
 app.use('/product', productRoutes);
+
+app.use((err, req, res, next) => {
+    logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    logger.error(err.stack);
+
+    res.status(err.status || 500).json({
+        error: 'Something went wrong! Please try again later.'
+    });
+});
 
 module.exports = app;
