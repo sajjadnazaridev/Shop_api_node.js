b053ef253b2f8132c7e8e00df7fd8d793a27d594
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-19 17:06:02 +0330

    [fix] Correct route for retrieving order history by ID

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/', orderController.createOrder);
 router.put('/:id', orderController.updateOrder);
-router.get('/', orderController.getOrderHIstory );
+router.get('/:id', orderController.getOrderHIstory );
 
 module.exports = router;
